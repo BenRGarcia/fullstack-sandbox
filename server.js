5f1703aed9792e7436e0fbe36c99cf97efe00776
Ben Garcia <ben@seebenprogram.com>
2018-04-19 20:18:47 -0400

    Build initial express server framework

---
+++ server.js
@@ -0,0 +1,21 @@
+// Require dependencies
+const express = require('express')
+const path = require('path')
+// Require routes
+const apiRouter = require('./routes/apiRoutes.js')
+// Instantiate express app
+const app = express()
+// Define server port
+const PORT = process.env.PORT || 8080
+
+// Mount middleware to parse request body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Define path of static assets
+app.use('/', express.static(path.join(__dirname, 'dist')))
+
+// Define path of RESTful API
+app.use('/api', apiRouter)
+
+app.listen(PORT, () => console.log(`Express server listening at PORT ${PORT}`))
