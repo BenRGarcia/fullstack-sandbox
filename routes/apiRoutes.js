c06e4dcd34f2dc674affc8d9b500208df8ffe0ec
Ben Garcia <ben@seebenprogram.com>
2018-04-19 20:55:28 -0400

    Add mock response data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,7 @@ const apiRouter = express.Router()
 // For debugging
 apiRouter.use((req, res, next) => {
   console.log(`${req.method} received at ${Date.now().toString()}`)
+  next()
 })
 
 // Define METHODS for path /api
@@ -14,13 +15,13 @@ apiRouter.route('/')
     res.send(`Server received your ${req.method} request`)
   })
   .post((req, res, next) => {
-    res.send(`Server received your ${req.method} request`)
+    res.send(`Server received your ${req.method} request, request body: ${req.body}`)
   })
   .put((req, res, next) => {
-    res.send(`Server received your ${req.method} request`)
+    res.send(`Server received your ${req.method} request, request body: ${req.body}`)
   })
   .delete((req, res, next) => {
-    res.send(`Server received your ${req.method} request`)
+    res.send(`Server received your ${req.method} request, request body: ${req.body}`)
   })
 
 module.exports = apiRouter
