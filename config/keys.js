af365204b2754b029f8f991e3f3a8277df7169c5
Ben Garcia <ben@seebenprogram.com>
2018-04-19 21:10:03 -0400

    Add dialect option to db key

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,8 @@ const db = {
   name: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  dialect: process.env.dialect
 }
 
 module.exports = {
