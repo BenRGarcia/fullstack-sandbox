91c1405a0ad6a8a53b4116f1c50c3347a7f3c876
Ben Garcia <ben@seebenprogram.com>
2018-04-19 21:26:51 -0400

    Correct typo capitalization

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@ const db = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  dialect: process.env.dialect
+  dialect: process.env.DB_DIALECT
 }
 
 module.exports = {
